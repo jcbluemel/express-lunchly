b08487e27d80c84b8834706f4689d85cfd423ead
Joel <jcbluemel@yahoo.com>
2022-07-01 11:24:17 -0700

    initial commit. added getFullName to customer class and updated templates to use

---
+++ db.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/** Database for lunchly */
+
+const { Client } = require("pg");
+
+const DB_URI = process.env.NODE_ENV === "test"
+    ? "postgresql:///lunchly_test"
+    : "postgresql:///lunchly";
+    // : "postgresql:///lunchly";
+
+
+let db = new Client({
+  connectionString: DB_URI,
+});
+
+db.connect();
+
+
+module.exports = db;
